3b16253ab683a5c85fa2a101406e9ed4d2c830b7
Sergio Azócar <sergioazoc@gmail.com>
2022-08-29 19:08:54 -0400

    add emits to input

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+  import { ref } from 'vue'
+
   const props = withDefaults(defineProps<{
     type: "text" | "email" | "password",
     placeholder: string,
@@ -9,15 +11,23 @@
     validFeedback: false,
     invalidFeedback: false
   })
+
+  const inputValue= ref('')
+
+  const emit = defineEmits<{
+    (e: 'inputValue', value: string): void
+  }>()
 </script>
 
 <template>
   <div class="input-group has-validation">
     <input
+      v-model="inputValue"
       class="form-control"
       :type="props.type"
       :placeholder="props.placeholder"
       required
+      @change="emit('inputValue', inputValue)"
     >
     <div
       v-if="props.validFeedback"
--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup>
-  import { ref } from 'vue'
+  import { ref, reactive } from 'vue'
   import BaseInput from '@/components/BaseInput.vue'
+
+  const formData = reactive({
+    text: '',
+    password: '',
+    email: ''
+  })
 
   const validateForm = ref(false)
 
   const onSubmit = () => {
-    console.log("form")
+    console.log(formData)
     validateForm.value = true
   }
 </script>
@@ -21,14 +27,17 @@
       type="text"
       valid-feedback="Texto correcto"
       invalid-feedback="Debes ingresar un texto"
+      @input-value="value => formData.text = value"
     />
     <BaseInput
       placeholder="ingresar un password"
       type="password"
+      @input-value="value => formData.password = value"
     />
     <BaseInput
       placeholder="ingresar un email"
       type="email"
+      @input-value="value => formData.email = value"
     />
     <button class="btn btn-primary">
       enviar
